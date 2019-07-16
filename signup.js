31f14623dedd25727235b3b88e16b9c1c12a0353
Jogeswararao Kolla <jogeskolla@gmail.com>
2019-07-16 00:36:58 -0700

    Updated the signup html

--- signup.js
+++ signup.js
@@ -12,7 +12,6 @@
   firebase.initializeApp(firebaseConfig);
 
   let firebaseStart=firebase.auth();
-
   let firebaseData=firebase.database();
    
   let userSignObj={};
@@ -20,6 +19,7 @@
 
 
   $(document).ready(function(eventObj){
+    
 
     $("#todoBtn").click(function(eventAddObj){
       console.log(eventAddObj);
@@ -28,70 +28,77 @@
       const user=firebaseStart.currentUser.uid;
   
       firebaseData.ref("users").child(user).push({todo:data,userID:user});
-      
       $("#todoId").val("");
+
+
       });
   
   
   
     $("#signinBtn").click(function(eventObj){
+      eventObj.preventDefault();
         console.log(eventObj);
-        userSignObj.email=$("#inputEmail").val();
-        userSignObj.password=$("#inputPassword").val();
+        userSignObj.email=$("#InputEmail1").val();
+        userSignObj.password=$("#InputPassword1").val();
         console.log(userSignObj);
         
   
-        firebase.auth().signInWithEmailAndPassword(userSignObj.email, userSignObj.password).then(function(eventObj){
-          console.log(eventObj);
+ firebase.auth().signInWithEmailAndPassword(userSignObj.email, userSignObj.password).then(function(sucessObj){
+          console.log(sucessObj);
+          console.log(sucessObj.user.emailVerified);
           console.log("Sign in Sucess");
-          window.location="signin.html";
+          // window.location="signin.html";
       }).catch(function(error) {
-        // Handle Errors here.
         var errorCode = error.code;
         var errorMessage = error.message;
         console.log("Sign in Error");
-        // ...
+        console.log(errorCode,errorMessage);
+        if(errorCode=="auth/wrong-password"){
+          console.log("Password is invalid");
+        }
+       $("#messageSignin").text(errorMessage);
       });
   
     });
   
     $("#signupBtn").click(function(eventObj){
         console.log(eventObj);
-        userSignUpObj.email=$("#inputEmail").val();
-        userSignUpObj.password=$("#inputPassword").val();
+        userSignUpObj.email=$("#InputEmail1").val().trim();
+        userSignUpObj.password=$("#InputPassword1").val();
        console.log(userSignUpObj);
        //clear input fields 
-       $("#inputEmail").val("");
-       $("#inputPassword").val("");
+       $("#InputEmail1").val("");
+       $("#InputPassword1").val("");
   
-       firebase.auth().createUserWithEmailAndPassword(userSignUpObj.email, userSignUpObj.password).
-       then(function(userSignUpObj){
+       firebase.auth().createUserWithEmailAndPassword(userSignUpObj.email, userSignUpObj.password)
+       .then(function(userSignUpObj){
            console.log(userSignUpObj);
        }).catch(function(error) {
           // Handle Errors here.
           var errorCode = error.code;
           var errorMessage = error.message;
-          console.log(errorCode,errorMessage);
+          if(errorCode=="auth/email-already-in-use")
+          {
+            console.log("User already exists");
+            console.log(errorCode,errorMessage);
+          }
+        
         });
   
     });
   
     $("#signoutBtn").click(function(eventObj){
-    console.log(eventObj);
-  
       firebase.auth().signOut().then(function(){
         console.log("Signout Sucessful");
         }).catch(function(err){
           console.log("Error Signout");
           console.log(err);
         });
-  
-        
     });
   
     $("#forgotPassword").on('click',function(){
   
-     let emailAddress= $("#inputEmail").val().trim();
+     let emailAddress= $("#InputEmail1").val().trim();
       firebase.auth().sendPasswordResetEmail(emailAddress).then(function() {
         // Email sent.
         console.log("password reset email sent..please check");
@@ -104,13 +111,11 @@
     });
   
   
-   
     firebase.auth().onAuthStateChanged(function(user) {
-  
       if (user) {
         // User is signed in.
       console.log(user);
-      // alert("user logged in " + user.uid);
+      console.log(user.uid);
       console.log(firebaseStart.currentUser.uid);
   
   
@@ -130,10 +135,8 @@
       } else {
         // User is signed out.
         console.log("sign out");
-        // ...
       }
     });
-
 
   });
 
